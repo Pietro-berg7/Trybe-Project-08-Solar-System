101070574d98276bb9b6b4d2152e10d529178ca6
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-24 00:18:57 -0300

    Requisito 7 concluído

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,7 +5,6 @@ import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
-    // const { planets } = this.props;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
